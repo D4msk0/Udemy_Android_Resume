4490e9ab474b4cb536193a6484dba69220c8be58
Merry Damsko <m.d4msk0@gmail.com>
2024-04-01 16:02:51 +0200

    finish App 2 functioning buttons - content - call - email
    scrollview

--- app/src/main/java/com/example/android/resume/MainActivity.java
+++ app/src/main/java/com/example/android/resume/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.resume;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button workHistoryButton;
+    Button callButton;
+    Button emailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +30,34 @@ public class MainActivity extends AppCompatActivity {
         });
 
         workHistoryButton = (Button) findViewById(R.id.workHistoryButton);
+        callButton = (Button) findViewById(R.id.callButton);
+        emailButton = (Button) findViewById(R.id.emailButton);
 
         workHistoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goToTheOtherActivity = new Intent(getApplicationContext(), WorkHistoryActivity.class);
                 startActivity(goToTheOtherActivity);
+            }
+        });
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri myPhoneNumber = Uri.parse("tel:+31649064211");
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, myPhoneNumber);
+                startActivity(callIntent);
+            }
+        });
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"m.d4msk0@gmail.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Maarten's Resume");
+                startActivity(emailIntent);
             }
         });
     }
